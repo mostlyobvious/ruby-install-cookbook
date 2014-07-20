9c9d2b37f964cc1e53a7b599090c598fb7dba1fb
Paweł Pacana <pawel.pacana@gmail.com>
2014-07-20 15:52:29 +0200

    Refactoring.

--- providers/default.rb
+++ providers/default.rb
@@ -1,8 +1,7 @@
 action :create do
   cache_dir            = Chef::Config[:file_cache_path]
-  archive_name         = "ruby-#{new_resource.name}.tar.bz2"
-  ruby_dir             = ::File.join(cache_dir, "ruby-#{new_resource.name}")
-  download_source      = "http://rvm.io/binaries/#{node[:platform]}/#{node[:platform_version]}/#{node[:kernel][:machine]}/#{archive_name}"
+  archive_name         = "ruby-#{ruby_version}.tar.bz2"
+  ruby_dir             = ::File.join(cache_dir, "ruby-#{ruby_version}")
   download_destination = ::File.join(cache_dir, archive_name)
 
   remote_file download_destination do
@@ -20,4 +19,12 @@ action :create do
     command "cp -r #{ruby_dir} #{new_resource.destination}"
     creates new_resource.destination
   end
+end
+
+def download_source
+  "http://rvm.io/binaries/#{node[:platform]}/#{node[:platform_version]}/#{node[:kernel][:machine]}/ruby-#{ruby_version}.tar.bz2"
+end
+
+def ruby_version
+  new_resource.name
 end
